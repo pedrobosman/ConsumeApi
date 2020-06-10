d7af81548914f3184776df9aaafdf19683c98717
Pedro Barros <pedrobosman@hotmail.com>
2020-06-09 21:02:34 -0300

    Colocando Projeto - Commit Unico

---
+++ ApiFetch/ICepApiService.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFetch
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<Address> GetAddressAsync(string cep);
+    }
+}
---
+++ ApiFetch/Program.cs
@@ -0,0 +1,33 @@
+﻿using Refit;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiFetch
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string cep = Console.ReadLine();
+
+            string baseUrl = "https://viacep.com.br";
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>(baseUrl);
+
+                Console.WriteLine("Consultando os dados do Cep: " + cep);
+
+                var address = await cepClient.GetAddressAsync(cep);
+
+                Console.WriteLine("Cidade: " + address.Localidade);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
